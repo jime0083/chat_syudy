564285ecf5f95a1098a596c7071fbeecd9065932
jime <kuritahajime.business@gmail.com>
2025-03-24 23:59:12 +0900

    ESlint,Vuetify 導入

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify'
 
 
 const app=createApp(App);
 app.use(router);
 app.mount('#app');
+app.use(vuetify);
+
+
